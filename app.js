7096c73b2a5d70916c81e8f46ec96a4afddda65b
Giorgi-Badzagua <104918680+Giorgi-Badzagua@users.noreply.github.com>
2022-10-24 15:40:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let nav = document.querySelector('nav');
+let ul = document.querySelector('nav ul')
+let under = document.querySelector('.underline');
+nav.addEventListener('mousemove', function (e) {
+
+    if (ul.getBoundingClientRect().left < e.clientX)
+        under.style.left = e.clientX + 'px'
+});
+
+
+
+
+
+let dropdowns = document.querySelectorAll('.lagndropdown');
+
+dropdowns.forEach(lagndropdown => {
+    let select = lagndropdown.querySelector('.select');
+    let caret = lagndropdown.querySelector('.caret');
+    let langs = lagndropdown.querySelector('.langs');
+    let optons = lagndropdown.querySelector('.langs li');
+    let selected = lagndropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        langs.classList.toggle('langs-open');
+    });
+
+    optopns.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            langs.classList.remove('langs-open');
+            option.forEach(option => {
+                option.clasList.remove('active');
+            }
+            );
+            option.clasList.add('active');
+        });
+    });
+
+
+});
+
+
+// swiper //
